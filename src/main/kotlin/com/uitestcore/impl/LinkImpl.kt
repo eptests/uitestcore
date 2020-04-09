141e66e6f104b3b5e6700958c8db342058ba69fc
elenapoduzova <elena.poduzova@gmail.com>
2020-04-09 16:41:33 +0300

    Improve Link element

--- src/main/kotlin/com/uitestcore/impl/LinkImpl.kt
+++ src/main/kotlin/com/uitestcore/impl/LinkImpl.kt
@@ -1,12 +1,16 @@
 package com.uitestcore.impl
 
-import com.uitestcore.elementobjects.Button
+import com.uitestcore.elementobjects.Link
 import org.openqa.selenium.By
 import org.openqa.selenium.WebElement
 
 internal class LinkImpl protected constructor(wrappedElement: WebElement?) :
-    AbstractElement(wrappedElement!!), Button {
+    AbstractElement(wrappedElement!!), Link {
     override fun click() {
         wrappedElement.findElement(By.tagName("a")).click()
+    }
+
+    override fun text(): String {
+        return wrappedElement.text
     }
 }
